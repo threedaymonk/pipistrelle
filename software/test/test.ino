27f705459fdc2f202ae8ad1aea6618b0f1d80be8
Paul Battley <pbattley@gmail.com>
2022-02-10 15:23:31 +0000

    Use fixed-point maths for waves
    In combination with the quadratic sine approximation, this is fast
    enough to calculate in real time.

--- software/test/test.ino
+++ software/test/test.ino
@@ -4,8 +4,15 @@
 #define SAMPLE_RATE 32000
 #define C2 65.4064
 
-double voctA, x, y, z;
-double voct = 0;
+typedef int32_t q14_t;
+#define Q14_1    0x4000
+#define Q14_1_4  0x1000
+#define Q14_1_2  0x2000
+#define Q14_3_4  0x3000
+
+#define blend(amt, a, b) (((Q14_1 - (amt)) * (a) + (amt) * (b)) / Q14_1)
+
+q14_t x, y, z;
 uint32_t __period = 100;
 
 void setup() {
@@ -20,14 +27,16 @@ void setup() {
 }
 
 void loop() {
-  static double frequency;
-  voct = readVoct() + floor(60.0L * unipolar(readPotA())) / 12.0L;
+  double frequency, voct;
+
+  // Quantise v/oct to nearest semitone.
+  voct = readVoct() + 5.0L * unipolar(readPotA());
   frequency = C2 * pow(2, voct);
   __period = SAMPLE_RATE / frequency;
 
-  x = constrain(unipolar(readPotB()) + bipolar(readCv1()) / 2, 0, 1);
-  y = constrain(unipolar(readPotC()) + bipolar(readCv2()) / 2, 0, 1);
-  z = unipolar(readPotD());
+  x = Q14_1 * constrain(unipolar(readPotB()) + bipolar(readCv1()) / 2, 0, 1);
+  y = Q14_1 * constrain(unipolar(readPotC()) + bipolar(readCv2()) / 2, 0, 1);
+  z = Q14_1 * unipolar(readPotD());
 
 
 
@@ -41,49 +50,65 @@ void loop() {
   Serial.print("\n");
   */
 
-  delay(10);
 }
 
-// Waveform functions are expressed as f(t) where 0 <= t < 1
+// Waveform functions take an input value of 0 <= x < Q14_1
+// and return a value in the range -Q14_1 <= 0 <= Q14_1
 
-double sineWave(double t) {
-  return sin(t * M_PI * 2);
+// Quadratic sine wave approximation at f = 1/x
+// See http://www.coranac.com/2009/07/sines/
+q14_t sineWave(q14_t x) {
+  int c, x2, y;
+  static const int qN= 13, qA= 12, B=19900, C=3516;
+
+  x <<= 1; // convert to Q15
+
+  c = x << (30 - qN);
+  x -= 1 << qN;
+
+  x = x << (31 - qN);
+  x = x >> (31 - qN);
+  x = x * x >> (2 * qN - 14);
+
+  y = B - (x * C >> 14);
+  y = (1 << qA) - (x * y >> 16);
+
+  y <<= 2; // convert from Q12 to Q14
+
+  return c >= 0 ? y : -y;
 }
 
-double triangleWave(double t) {
-  if (t < 0.25) return t * 4;
-  if (t < 0.75) return 1 - (t - 0.25) * 4;
-  return 1 + (t - 1.25) * 4;
+// Triangle wave at f = 1/x
+q14_t triangleWave(q14_t x) {
+  if (x < Q14_1_4) return x * 4;
+  if (x < Q14_3_4) return Q14_1 - (x - Q14_1_4) * 4;
+  return Q14_1 + (x - Q14_1 - Q14_1_4) * 4;
 }
 
-double squareWave(double t) {
-  if (t < 0.5) return 1;
-  return -1;
+// Square wave at f = 1/x
+q14_t squareWave(q14_t x) {
+  if (x < Q14_1_2) return Q14_1;
+  return -Q14_1;
 }
 
-double sawWave(double t) {
-  if (t < 0.5) return t * 2;
-  return t * 2 - 2;
+// Saw wave at f = 1/x
+q14_t sawWave(q14_t x) {
+  if (x < Q14_1_2) return x * 2;
+  return x * 2 - 2 * Q14_1;
 }
 
 void TC4_Handler() {
   static uint32_t offset = 0, period;
-  static double sample = 0;
-  static double time, frequency;
+  static q14_t t, sample;
 
-  if (offset == 0) {
-    period = __period;
-  }
+  if (offset == 0) period = __period;
 
-  /*
-  time = offset * frequency / SAMPLE_RATE;
-  sample = 
-    (1 - y) * ((1 - x) * sineWave(time)   + x * triangleWave(time))
-    + y     * ((1 - x) * squareWave(time) + x * sawWave(time));
-  DACWrite(511.5 + 511.5 * sample);
-  */
-  if (2 * offset > period) DACWrite(1023);
-  else DACWrite(0);
+  t = (offset * Q14_1) / period;
+
+  sample = blend(y, blend(x, sineWave(t), triangleWave(t)),
+                    blend(x, squareWave(t), sawWave(t)));
+
+  DACWrite(constrain((sample + Q14_1) >> 5, 0, 1023));
 
   offset += 1;
   if (offset >= period) offset = 0;
