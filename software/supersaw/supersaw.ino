f61ed187364c45763720f9e17586677d4e356b26
Paul Battley <pbattley@gmail.com>
2022-02-13 19:41:22 +0000

    Supersaw!

---
+++ software/supersaw/supersaw.ino
@@ -0,0 +1,70 @@
+// Supersaw
+//
+// Pot A => Coarse frequency
+// Pot B => Fine frequency
+// Pot C + CV 1 => Spread
+// Pot D + CV 2 => Sub oscillator
+
+#include <pipistrelle.h>
+#include <calibration.h>
+#include <math.h>
+#include <q14.h>
+#define SAMPLE_RATE 48000
+#define C0 16.3516
+
+uint32_t period[] = {1, 1, 1, 1, 1, 1, 1, 1};
+q14_t subosc = 0;
+
+void setup() {
+  uint32_t top;
+  initialize_hardware(SAMPLE_RATE);
+  digitalWrite(LED, 1); // Shows that we got this far
+}
+
+void loop() {
+  double frequency, voct, spread;
+  int led_change_at = 0;
+
+  voct = read_voct()
+    + 6.0L * unipolar(read_pota()) // coarse tuning C0 + 6 octaves
+    + unipolar(read_potb()) / 6.0L; // fine tuning +/- 2 semitones
+
+  frequency = C0 * pow(2, voct);
+  spread = 0.02 * frequency * constrain(unipolar(read_potc()) + bipolar(read_cv1()) / 2, 0, 1);
+  subosc = Q14_1 * constrain(unipolar(read_potd()) + bipolar(read_cv2()) / 2, 0, 1);
+
+  for (int i = 0; i < 7; i++) {
+    period[i] = SAMPLE_RATE / (frequency + spread * (i - 3));
+  }
+
+  // Oscillator 7 is sub
+  period[7] = 4 * SAMPLE_RATE / frequency;
+}
+
+void TC4_Handler() {
+  static uint32_t offset[] = {0, 0, 0, 0, 0, 0, 0}, cycle_period[7];
+  static q14_t t[7];
+  q14_t sample = 0;
+
+  for (int i = 0; i < 8; i++) {
+    // Set the period for this cycle.
+    // t == 0 is zero crossing point for all waveforms, so if we only ever
+    // change frequency here we won't get any audible artefacts from shifting.
+    if (offset[i] == 0) cycle_period[i] = period[i];
+
+    // t is the relative offset within the cycle in Q14 representation
+    t[i] = (offset[i] * Q14_1) / cycle_period[i];
+
+    if (i == 7)
+      sample += (subosc * q14_quarter_square(t[i])) / Q14_1;
+    else
+      sample += q14_saw(t[i]);
+
+    offset[i] += 1;
+    if (offset[i] >= cycle_period[i]) offset[i] = 0;
+  }
+
+  q14_dac_write(sample / 7);
+
+  REG_TC4_INTFLAG = TC_INTFLAG_OVF; // clear interrupt overflow flag
+}
