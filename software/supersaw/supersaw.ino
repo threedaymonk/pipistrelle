bf6cd9e0807c605e89662a3c62a88ab00bdcbe31
Paul Battley <pbattley@gmail.com>
2022-02-15 19:45:42 +0000

    We can only manage 5 saws before glitching

--- software/supersaw/supersaw.ino
+++ software/supersaw/supersaw.ino
@@ -9,14 +9,19 @@
 #include <calibration.h>
 #include <math.h>
 #include <q14.h>
-#define SAMPLE_RATE 48000
+#define SAMPLE_RATE 32000
 #define C0 16.3516
+#define OSCILLATORS 5
 
-uint32_t period[] = {1, 1, 1, 1, 1, 1, 1, 1};
+uint32_t period[OSCILLATORS];
 q14_t subosc = 0;
 
 void setup() {
   uint32_t top;
+
+  // Anything is fine as long as it's not zero
+  for (int i = 0; i < OSCILLATORS; i++) period[i] = 1000;
+
   initialize_hardware(SAMPLE_RATE);
   digitalWrite(LED, 1); // Shows that we got this far
 }
@@ -35,12 +40,12 @@ void loop() {
   subosc = Q14_1
          * constrain(unipolar(read_potd()) + bipolar(read_cv2()) / 2, 0, 1);
 
-  for (int i = 0; i < 7; i++) {
-    period[i] = SAMPLE_RATE / (frequency + spread * (i - 3));
+  for (int i = 0; i < OSCILLATORS; i++) {
+    period[i] = SAMPLE_RATE / (frequency + spread * (i - OSCILLATORS / 2));
   }
 
   // Oscillator 7 is sub
-  period[7] = 4 * SAMPLE_RATE / frequency;
+  //period[7] = 4 * SAMPLE_RATE / frequency;
 }
 
 void TC4_Handler() {
@@ -48,7 +53,7 @@ void TC4_Handler() {
   static q14_t t[7];
   q14_t sample = 0;
 
-  for (int i = 0; i < 8; i++) {
+  for (int i = 0; i < OSCILLATORS; i++) {
     // Set the period for this cycle.
     // t == 0 is zero crossing point for all waveforms, so if we only ever
     // change frequency here we won't get any audible artefacts from shifting.
@@ -57,16 +62,16 @@ void TC4_Handler() {
     // t is the relative offset within the cycle in Q14 representation
     t[i] = (offset[i] * Q14_1) / cycle_period[i];
 
-    if (i == 7)
-      sample += (subosc * q14_quarter_square(t[i])) / Q14_1;
-    else
+    //if (i == 7)
+    //  sample += (subosc * q14_quarter_square(t[i])) / Q14_1;
+    //else
       sample += q14_saw(t[i]);
 
     offset[i] += 1;
     if (offset[i] >= cycle_period[i]) offset[i] = 0;
   }
 
-  q14_dac_write(sample / 7);
+  q14_dac_write(sample / OSCILLATORS);
 
   REG_TC4_INTFLAG = TC_INTFLAG_OVF; // clear interrupt overflow flag
 }
