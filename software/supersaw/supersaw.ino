1784da050d761a4eccae0c5ccd0f8382b56f729c
Paul Battley <pbattley@gmail.com>
2022-02-16 18:42:28 +0000

    Pot D is side oscillator mix

--- software/supersaw/supersaw.ino
+++ software/supersaw/supersaw.ino
@@ -15,6 +15,7 @@
 #define BUFSIZE 1024
 
 uint32_t period[OSCILLATORS];
+q14_t oscmix[OSCILLATORS];
 q14_t buffer[BUFSIZE];
 int rptr = 0, wptr = 1;
 
@@ -34,7 +35,7 @@ void setup() {
 q14_t next_sample() {
   static uint32_t offset[] = {0, 0, 0, 0, 0, 0, 0}, cycle_period[7];
   static q14_t t[7];
-  q14_t sample = 0;
+  q14_t sample = 0, divisor = 0;
 
   for (int i = 0; i < OSCILLATORS; i++) {
     // Set the period for this cycle.
@@ -45,13 +46,14 @@ q14_t next_sample() {
     // t is the relative offset within the cycle in Q14 representation
     t[i] = (offset[i] * Q14_1) / cycle_period[i];
 
-    sample += q14_saw(t[i]);
+    sample += (q14_saw(t[i]) * oscmix[i]) / Q14_1;
+    divisor += oscmix[i];
 
     offset[i] += 1;
     if (offset[i] >= cycle_period[i]) offset[i] = 0;
   }
 
-  return sample / OSCILLATORS;
+  return sample / (divisor / Q14_1);
 }
 
 void fill_buffer() {
@@ -62,20 +64,23 @@ void fill_buffer() {
 }
 
 void loop() {
-  double frequency, voct, spread;
+  double frequency, voct, detune, sidemix;
   int led_change_at = 0;
 
   voct = read_voct(HIGH_ACCURACY)
     + 6.0L * unipolar(read_pota(HIGH_ACCURACY)) // coarse tuning C0 + 6 octaves
-    + unipolar(read_potb(HIGH_ACCURACY)) / 6.0L; // fine tuning +/- 2 semitones
+    + unipolar(read_potb(HIGH_ACCURACY)) / 2.0L; // fine tuning +/- 1/2 oct
 
   frequency = C0 * pow(2, voct);
-  spread = frequency / 80
+  detune = frequency / 80
          * constrain(unipolar(read_potc(LOW_ACCURACY))
                      + bipolar(read_cv1(LOW_ACCURACY)) / 2, 0, 1);
+  sidemix = constrain(unipolar(read_potd(LOW_ACCURACY))
+                      + bipolar(read_cv2(LOW_ACCURACY)) / 2, 0, 1);
 
-  for (int i = 0; i < OSCILLATORS; i++) {
-    period[i] = SAMPLE_RATE / (frequency + spread * (i - OSCILLATORS / 2));
+  for (int i = 0, j; i < OSCILLATORS; i++) {
+    period[i] = SAMPLE_RATE / (frequency + detune * (i - OSCILLATORS / 2));
+    oscmix[i] = Q14_1 - Q14_1 * pow((1 - sidemix) * 0.75, abs(OSCILLATORS / 2 - i));
   }
 
   fill_buffer();
