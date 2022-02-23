e98a249d185e457a6932fd27624bb4765200a825
Paul Battley <pbattley@gmail.com>
2022-02-23 11:27:56 +0000

    Reformat ADSR code
    And correct typos of ADRS to ADSR

--- software/adsr/src/ADSR.cpp
+++ software/adsr/src/ADSR.cpp
@@ -13,11 +13,15 @@
 //
 //  This source code is provided as is, without warranty.
 //  You may copy and distribute verbatim copies of this document.
-//  You may modify and use this source code to create binary code for your own purposes, free or commercial.
+//  You may modify and use this source code to create binary code for your own
+//  purposes, free or commercial.
 //
-//  1.01  2016-01-02  njr   added calcCoef to SetTargetRatio functions that were in the ADSR widget but missing in this code
-//  1.02  2017-01-04  njr   in calcCoef, checked for rate 0, to support non-IEEE compliant compilers
-//  1.03  2020-04-08  njr   changed float to double; large target ratio and rate resulted in exp returning 1 in calcCoef
+//  1.01  2016-01-02  njr   added calcCoef to SetTargetRatio functions that
+//                          were in the ADSR widget but missing in this code
+//  1.02  2017-01-04  njr   in calcCoef, checked for rate 0, to support
+//                          non-IEEE compliant compilers
+//  1.03  2020-04-08  njr   changed float to double; large target ratio and
+//                          rate resulted in exp returning 1 in calcCoef
 //
 
 #include "ADSR.h"
@@ -25,59 +29,59 @@
 
 
 ADSR::ADSR(void) {
-    reset();
-    setAttackRate(0);
-    setDecayRate(0);
-    setReleaseRate(0);
-    setSustainLevel(1.0);
-    setTargetRatioA(0.3);
-    setTargetRatioDR(0.0001);
+  reset();
+  setAttackRate(0);
+  setDecayRate(0);
+  setReleaseRate(0);
+  setSustainLevel(1.0);
+  setTargetRatioA(0.3);
+  setTargetRatioDR(0.0001);
 }
 
 ADSR::~ADSR(void) {
 }
 
 void ADSR::setAttackRate(double rate) {
-    attackRate = rate;
-    attackCoef = calcCoef(rate, targetRatioA);
-    attackBase = (1.0 + targetRatioA) * (1.0 - attackCoef);
+  attackRate = rate;
+  attackCoef = calcCoef(rate, targetRatioA);
+  attackBase = (1.0 + targetRatioA) * (1.0 - attackCoef);
 }
 
 void ADSR::setDecayRate(double rate) {
-    decayRate = rate;
-    decayCoef = calcCoef(rate, targetRatioDR);
-    decayBase = (sustainLevel - targetRatioDR) * (1.0 - decayCoef);
+  decayRate = rate;
+  decayCoef = calcCoef(rate, targetRatioDR);
+  decayBase = (sustainLevel - targetRatioDR) * (1.0 - decayCoef);
 }
 
 void ADSR::setReleaseRate(double rate) {
-    releaseRate = rate;
-    releaseCoef = calcCoef(rate, targetRatioDR);
-    releaseBase = -targetRatioDR * (1.0 - releaseCoef);
+  releaseRate = rate;
+  releaseCoef = calcCoef(rate, targetRatioDR);
+  releaseBase = -targetRatioDR * (1.0 - releaseCoef);
 }
 
 double ADSR::calcCoef(double rate, double targetRatio) {
-    return (rate <= 0) ? 0.0 : exp(-log((1.0 + targetRatio) / targetRatio) / rate);
+  return (rate <= 0) ? 0.0 : exp(-log((1.0 + targetRatio) / targetRatio) / rate);
 }
 
 void ADSR::setSustainLevel(double level) {
-    sustainLevel = level;
-    decayBase = (sustainLevel - targetRatioDR) * (1.0 - decayCoef);
+  sustainLevel = level;
+  decayBase = (sustainLevel - targetRatioDR) * (1.0 - decayCoef);
 }
 
 void ADSR::setTargetRatioA(double targetRatio) {
-    if (targetRatio < 0.000000001)
-        targetRatio = 0.000000001;  // -180 dB
-    targetRatioA = targetRatio;
-    attackCoef = calcCoef(attackRate, targetRatioA);
-    attackBase = (1.0 + targetRatioA) * (1.0 - attackCoef);
+  if (targetRatio < 0.000000001)
+    targetRatio = 0.000000001;  // -180 dB
+  targetRatioA = targetRatio;
+  attackCoef = calcCoef(attackRate, targetRatioA);
+  attackBase = (1.0 + targetRatioA) * (1.0 - attackCoef);
 }
 
 void ADSR::setTargetRatioDR(double targetRatio) {
-    if (targetRatio < 0.000000001)
-        targetRatio = 0.000000001;  // -180 dB
-    targetRatioDR = targetRatio;
-    decayCoef = calcCoef(decayRate, targetRatioDR);
-    releaseCoef = calcCoef(releaseRate, targetRatioDR);
-    decayBase = (sustainLevel - targetRatioDR) * (1.0 - decayCoef);
-    releaseBase = -targetRatioDR * (1.0 - releaseCoef);
+  if (targetRatio < 0.000000001)
+    targetRatio = 0.000000001;  // -180 dB
+  targetRatioDR = targetRatio;
+  decayCoef = calcCoef(decayRate, targetRatioDR);
+  releaseCoef = calcCoef(releaseRate, targetRatioDR);
+  decayBase = (sustainLevel - targetRatioDR) * (1.0 - decayCoef);
+  releaseBase = -targetRatioDR * (1.0 - releaseCoef);
 }
